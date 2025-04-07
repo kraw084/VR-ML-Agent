b9e16dc85299e90c9d4f1ab913b9408fb84893ae
kraw084 <kraw084@aucklanduni.ac.nz>
2025-04-07 16:58:30 +1200

    strip uneeded columns

--- data_utils/format_vrnet2.py
+++ data_utils/format_vrnet2.py
@@ -7,4 +7,11 @@ dataset_path = os.getenv("DATASET_PATH")
 
 csv = pd.read_csv(f"{dataset_path}/15_2/15_data.csv")
 
+cols_to_remove = ["timestamp", "object_name", "mesh_name", "bounds", "m_matrix", "object_id", "mesh_id",
+                  "static", "camera_name", "p_matrix", "v_matrix", "camera_id", "is_main", 
+                  "ConnectedControllerTypes", "ControllerMask"]
+cols_to_remove += [c for c in csv.columns if "acc" in c]
+
+csv = csv.drop(columns = cols_to_remove)
+
 print(csv.columns)
