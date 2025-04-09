4f1736c885f58b7399c2624ebe99bd9d393baada
kraw084 <kraw084@aucklanduni.ac.nz>
2025-04-09 13:58:05 +1200

    image and csv formatting

--- data_utils/format_vrnet2_csvs.py
+++ data_utils/format_vrnet2_csvs.py
@@ -1,19 +1,43 @@
-import dotenv
 import os
+
+import dotenv
 import pandas as pd
+from tqdm import tqdm
 
 dotenv.load_dotenv()
 dataset_path = os.getenv("DATASET_PATH")
 
-csv = pd.read_csv(f"{dataset_path}/15_2/15_data.csv")
 
-cols_to_remove = ["timestamp", "object_name", "mesh_name", "bounds", "m_matrix", "object_id", "mesh_id",
+for session in os.listdir(dataset_path):
+    #read csv
+    print(f"Formatting {session} data")
+    first_num = session.split("_")[0]
+    csv = pd.read_csv(f"{dataset_path}/{session}/{first_num}_data.csv")
+    csv_length = len(csv)
+    
+    #remove uneeded columns
+    cols_to_remove = ["timestamp", "object_name", "mesh_name", "bounds", "m_matrix", "object_id", "mesh_id",
                   "static", "camera_name", "p_matrix", "v_matrix", "camera_id", "is_main", 
                   "ConnectedControllerTypes", "ControllerMask"]
-cols_to_remove += [c for c in csv.columns if "acc" in c]
+    cols_to_remove += [c for c in csv.columns if "acc" in c]    
+    csv = csv.drop(columns = cols_to_remove)
 
-csv = csv.drop(columns = cols_to_remove)
-
-print(csv.columns)
-
-#Need to add identifier column
+    #rename a column and add a session column
+    csv = csv.rename(columns={"key":"frame"})
+    csv.insert(0, "session", [session] * len(csv))
+    
+    #find all frame numbers for this session
+    im_names = os.listdir(f"{dataset_path}/{session}/video")
+    im_names = [int(n[:-4]) for n in im_names]
+    
+    #remove rows with no images
+    bool_mask = (csv["frame"].isin(im_names))
+    csv = csv[bool_mask]
+            
+    print(f"Removed {csv_length - bool_mask.sum()} rows")
+    print("Finished formatting")
+    
+    #save as a new csv
+    csv.to_csv(f"{dataset_path}/{session}/{session}_data.csv")
+    
+    print()
--- data_utils/format_vrnet2_images.py
+++ data_utils/format_vrnet2_images.py
@@ -1,23 +1,31 @@
 import os
 import zipfile
-import cv2
+import shutil
 
+from tqdm import tqdm
+import cv2
 import dotenv
 
 dotenv.load_dotenv()
 dataset_path = os.getenv("DATASET_PATH")
 
 for session in os.listdir(dataset_path):
-    print(f"Extracting {session.split("_")[0]}_video.zip")
+    print(f"Extracting {session.split('_')[0]}_video.zip")
     
-    if not os.path.exists(f"{dataset_path}/frames"): os.mkdir(f"{dataset_path}/frames")
-    
-    with zipfile.ZipFile(f"{dataset_path}/{session.split("_")[0]}_video.zip","r") as zip_ref:
-        zip_ref.extractall(f"{dataset_path}/frames") #TEST WHERE THIS GOES
+    identifier = session.split("_")[0]
+    session_folder = f"{dataset_path}/{session}/"
+        
+    with zipfile.ZipFile(f"{session_folder}/{identifier}_video.zip","r") as zip_file:
+        zip_file.extractall(f"{session_folder}") 
+        
+    shutil.rmtree(f"{session_folder}/__MACOSX")
         
     print("Finished unzipping")
     
-    for im_path in os.listdir(f"{dataset_path}/frames"):
-       pass 
-        
-    break
+    for im_path in tqdm(os.listdir(f"{session_folder}/video"), desc="Resizing images"):
+       im = cv2.imread(f"{session_folder}/video/{im_path}")
+       im = cv2.resize(im, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+       cv2.imwrite(f"{session_folder}/video/{im_path[1:]}", im)
+       os.remove(f"{session_folder}/video/{im_path}") 
+    
+    print()
