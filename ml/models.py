dde5be402e10cd9dbc8b7b55f517fbc57a8e2a95
kraw084 <kraw084@aucklanduni.ac.nz>
2025-04-23 10:02:38 +1200

    Larger model training

--- ml/models.py
+++ ml/models.py
@@ -5,17 +5,18 @@ import torchvision
 from efficientnet_pytorch import EfficientNet
 
 class EfficientnetAgent(torch.nn.Module):
-    def __init__(self, im_size = (288, 288), output_size = 10, pretrained = True):
+    def __init__(self, im_size = (288, 288), output_size = 10, pretrained = True, size=2):
         #self.model = efficientnet_b2(num_classes = 1000, weights = None if not pretrained else EfficientNet_B2_Weights.IMAGENET1K_V1)
         super().__init__()
         
         if pretrained:
-            self.model = EfficientNet.from_pretrained("efficientnet-b2") 
+            self.model = EfficientNet.from_pretrained(f"efficientnet-b{size}") 
         else:
-            self.model = EfficientNet.from_name("efficientnet-b2") 
+            self.model = EfficientNet.from_name(f"efficientnet-b{size}") 
             
         self.model._fc = torch.nn.Linear(self.model._fc.in_features, output_size)
         
+        self.size = size
         self.im_size = im_size
         self.output_size = output_size
         
--- run_training.py
+++ run_training.py
@@ -24,29 +24,29 @@ val_dataset = VRNET2_Multi_Session_Dataset("val",
                                             transform=x_transform)
 
 #define model
-model = EfficientnetAgent(im_size=im_size, output_size=10)
+model = EfficientnetAgent(im_size=im_size, output_size=10, size=4)
 
 #hyperparmaters
-lr = 1e-4
+lr = 1e-5
 loss_func = torch.nn.MSELoss()
-weight_decay = 1e-3
+weight_decay = 1e-4
 
 #select device
-gpu_num = 0
+gpu_num = 7
 device = torch.device(f'cuda:{gpu_num}')
 print(f"Using device {gpu_num}: {torch.cuda.get_device_properties(gpu_num).name}")
 
 #setup training 
-trainer = Trainer(name = "ENB2_MSE",
+trainer = Trainer(name = "ENB4_MSE",
                   model = model,
                   train_dataset = train_dataset,
                   val_dataset = val_dataset,
-                  epochs = 100,
-                  batch_size = 128,
+                  epochs = 30,
+                  batch_size = 64,
                   lr = lr,
                   loss_fn = loss_func,
                   weight_decay = weight_decay,
-                  save_freq = 10,
+                  save_freq = 5,
                   device = device,
                   workers = 32
                   )
